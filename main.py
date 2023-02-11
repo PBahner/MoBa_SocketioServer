9a970d098050eaff0dff23f0ea8a9f8a0ca50cf2
PBahner <kl.timo@outlook.de>
2023-02-11 23:52:24 +0100

    adding functionality for sending switch positions on connect / on change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 sio = socketio.Server(cors_allowed_origins='*') #, logger=True, engineio_logger=True
 app = socketio.WSGIApp(sio)
 
-CAN_ENABLED = True
+CAN_ENABLED = False
 
 if CAN_ENABLED:
     import can
@@ -46,20 +46,18 @@ class CanCommunicator:
     @staticmethod
     def send_switch_states():
         data = [s.target_pos for s in switches]
-        data.insert(5, False)
-        data.insert(5, False)
-        data.insert(5, False)
         print(data)
         data = CanCommunicator.bools_to_bytes(data)
         print("data", data)
         if CanCommunicator.send_msg(1, data):
             for switch in switches:
+                # only for testing
                 switch.current_pos = switch.target_pos
 
     @staticmethod
     def send_msg(id, data):
         if not CAN_ENABLED:
-            return
+            return True
         bustype = 'socketcan'
         channel = 'can0'
         with can.interface.Bus(channel=channel, bustype=bustype, bitrate=125000) as bus:
@@ -76,15 +74,23 @@ class CanCommunicator:
 class Esp32Communicator(socketio.Namespace):
     def on_connect(self, sid, environ, auth):
         print('connect ', sid)
+        data = {switch.id: switch.current_pos for switch in switches}
+        sio.emit("init_switch_positions", {'data': data})
 
     def on_disconnect(self, sid):
         print('disconnect ', sid)
 
     def on_change_switches(self, sid, data):
+        response_data = {}
         for switch in data["data"]:
             switches[switch].change_switch_state()
             print("Weiche " + str(switch) + " stellen")
         CanCommunicator.send_switch_states()
+        for switch in data["data"]:
+            # response data only for testing
+            response_data[switches[switch].id] = switches[switch].target_pos
+        print("response: ", response_data)
+        sio.emit("update_switch_positions", {'data': response_data})
         # sio.emit("state_change_request", {'data': data})
 
 
