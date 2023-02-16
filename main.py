d66b41f5cc0d3158434df5d1593fc43d18511425
PBahner <kl.timo@outlook.de>
2023-02-16 21:52:14 +0100

    adding id for servo-module, sending can-data by servo-module-id

--- main.py
+++ main.py
@@ -12,16 +12,23 @@ if CAN_ENABLED:
 
 
 class Switches:
-    def __init__(self, id):
+    def __init__(self, id, servo_module):
         self.current_pos = False
         self.target_pos = False
         self.id = id
+        self.servo_module = servo_module
 
     def change_switch_state(self):
         self.target_pos = not self.current_pos
 
 
-switches = [Switches(0), Switches(1), Switches(2), Switches(3), Switches(4), Switches(5), Switches(6)]
+switches = [Switches(0, 0),
+            Switches(1, 0),
+            Switches(2, 0),
+            Switches(3, 1),
+            Switches(4, 1),
+            Switches(5, 0),
+            Switches(6, 0)]
 
 
 class CanCommunicator:
@@ -39,12 +46,13 @@ class CanCommunicator:
 
     @staticmethod
     def send_switch_states():
-        data = [s.target_pos for s in switches]
-        data.insert(5, False)
-        data.insert(5, False)
-        data.insert(5, False)
+        switch_positions = [(s.servo_module, s.target_pos) for s in switches]
+        print(switch_positions)
+        data = [[] for _ in range(max(p[0] for p in switch_positions) + 1)]
+        for module, pos in switch_positions:
+            data[module].append(pos)
         print(data)
-        data = CanCommunicator.bools_to_bytes(data)
+        data = [CanCommunicator.bools_to_bytes(p)[0] for p in data]
         print("data", data)
         if CanCommunicator.send_msg(1, data):
             for switch in switches:
