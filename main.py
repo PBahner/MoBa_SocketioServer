ef18b9e31e10d2c882b782dbada62b8335141d36
PBahner <kl.timo@outlook.de>
2023-11-05 11:16:11 +0100

    implement switch feedback - create class 'PcfPinRef' to store all required information to access a pcf-input - update class 'Switches' to handle feedbacks - create function 'request_inputs' on CAN-bus - create function 'receive_event' on CAN-bus (+notifier if data is available) - create function 'send_switch_states' on socketio - create function 'updater' for repetitive tasks

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
+import threading
 import socketio
 import eventlet
-import time
+eventlet.monkey_patch()
 
 sio = socketio.Server(cors_allowed_origins='*') #, logger=True, engineio_logger=True
 app = socketio.WSGIApp(sio)
 
-CAN_ENABLED = False
+CAN_ENABLED = True
 
 if CAN_ENABLED:
     import can
+
+
+class PcfPinRef:  # contains all required information/addresses to access an PCF8574 input
+    def __init__(self,
+                 input_module,
+                 input_type,
+                 input_address,
+                 input_pin):
+        self.input_module = input_module
+        self.input_type = input_type
+        self.input_address = input_address
+        self.input_pin = input_pin
+
+    def compare(self, input_module, input_type, input_address):
+        return (input_module == self.input_module and
+                input_type == self.input_type and
+                input_address == self.input_address)
 
 
 class Switches:
@@ -18,28 +36,55 @@ class Switches:
         "": 2,
     }
 
-    def __init__(self, switch_id, servo_module):
-        self.current_pos = self.__pos["+"]
-        self.target_pos = self.__pos["+"]
+    def __init__(self, switch_id,
+                 servo_module,
+                 input_reference_plus=None,
+                 input_reference_minus=None):
+        self.__input_pin_minus = None
+        self.__input_pin_plus = None
+        self.current_pos = self.__pos[""]
+        self.target_pos = self.__pos[""]
         self.id = switch_id
         self.servo_module = servo_module
+        self.input_reference_plus = input_reference_plus
+        self.input_reference_minus = input_reference_minus
+
+    def set_input_pin_minus(self, input_pin_minus):
+        self.__input_pin_minus = input_pin_minus
+        self.__update_current_pos()
+
+    def set_input_pin_plus(self, input_pin_plus):
+        self.__input_pin_plus = input_pin_plus
+        self.__update_current_pos()
+
+    def __update_current_pos(self):
+        if self.__input_pin_minus == True and self.__input_pin_plus == False:
+            self.current_pos = self.__pos["-"]
+        elif self.__input_pin_minus == False and self.__input_pin_plus == True:
+            self.current_pos = self.__pos["+"]
+        else:
+            self.current_pos = self.__pos[""]
 
     def change_switch_state(self):
         self.target_pos = not self.current_pos
 
 
-switches = [Switches(0, 0),
-            Switches(1, 0),
-            Switches(2, 0),
+switches = [Switches(0, 0, PcfPinRef(0, 2, 0x39, 0), PcfPinRef(0, 2, 0x39, 1)),
+            Switches(1, 0, PcfPinRef(0, 2, 0x38, 7), PcfPinRef(0, 2, 0x38, 6)),
+            Switches(2, 0, PcfPinRef(0, 2, 0x38, 4), PcfPinRef(0, 2, 0x38, 5)),
             Switches(3, 1),
             Switches(4, 1),
-            Switches(5, 0),
-            Switches(6, 0)]
+            Switches(5, 0, PcfPinRef(0, 2, 0x38, 2), PcfPinRef(0, 2, 0x38, 3)),
+            Switches(6, 0, PcfPinRef(0, 2, 0x38, 1), PcfPinRef(0, 2, 0x38, 0))]
 
 
 class CanCommunicator:
+    bustype = 'socketcan'
+    channel = 'can0'
+
     @staticmethod
     def bools_to_bytes(list):
+        list = [b == 1 for b in list]
         output_byte = [0]
         byte_number = 0
         for i, x in enumerate(list):
@@ -51,45 +96,70 @@ class CanCommunicator:
         return output_byte
 
     @staticmethod
+    def request_inputs(module_id, input_type, input_address):
+        data = [module_id + input_type << 4, input_address]
+        CanCommunicator.send_msg(12, data)
+
+    @staticmethod
     def send_switch_states():
         switch_positions = [(s.servo_module, s.target_pos) for s in switches]
-        print(switch_positions)
         data = [[] for _ in range(max(p[0] for p in switch_positions) + 1)]
         for module, pos in switch_positions:
             data[module].append(pos)
-        print(data)
         data = [CanCommunicator.bools_to_bytes(p)[0] for p in data]
-        print("data", data)
         if CanCommunicator.send_msg(1, data):
             for switch in switches:
                 # only for testing
-                switch.current_pos = switch.target_pos
+                if switch.input_reference_minus is None or switch.input_reference_plus is None or not CAN_ENABLED:
+                    switch.current_pos = switch.target_pos
 
     @staticmethod
     def send_msg(id, data):
         if not CAN_ENABLED:
             return True
-        bustype = 'socketcan'
-        channel = 'can0'
-        with can.interface.Bus(channel=channel, bustype=bustype, bitrate=125000) as bus:
+        with can.interface.Bus(channel=CanCommunicator.channel,
+                               bustype=CanCommunicator.bustype,
+                               bitrate=125000) as bus:
             msg = can.Message(arbitration_id=id, data=data, is_extended_id=False)
             try:
                 bus.send(msg)
-                print(f"Message sent on {bus.channel_info}")
+                print(f"[CAN] Message sent on {bus.channel_info} id: {msg.arbitration_id} msg: {msg.data}")
                 return True
             except can.CanError:
-                print("Message NOT sent")
+                print("[CAN] Message NOT sent")
                 return False
+
+    @staticmethod
+    def receive_event(msg):
+        if msg.arbitration_id == 11:
+            module_id = msg.data[0] & 0x0F
+            input_type = msg.data[0] >> 4
+            input_address = msg.data[1]
+            for switch in switches:
+                if switch.input_reference_plus is None or switch.input_reference_plus is None:
+                    continue
+                if switch.input_reference_minus.compare(module_id, input_type, input_address):
+                    input_pin = (msg.data[2] >> switch.input_reference_minus.input_pin) & 1
+                    switch.set_input_pin_minus(input_pin)
+                if switch.input_reference_plus.compare(module_id, input_type, input_address):
+                    input_pin = (msg.data[2] >> switch.input_reference_plus.input_pin) & 1
+                    switch.set_input_pin_plus(input_pin)
+
+            response_data = {switch.id: switch.current_pos for switch in switches}
+            print("[ESP] emit: update_switch_positions", response_data)
+            sio.emit("update_switch_positions", {'data': response_data})
 
 
 class Esp32Communicator(socketio.Namespace):
     def on_connect(self, sid, environ, auth):
-        print('connect ', sid)
+        print('[ESP] connect ', sid)
         data = {switch.id: switch.current_pos for switch in switches}
         sio.emit("init_switch_positions", {'data': data})
+        print("[ESP] emit: init_switch_positions", data)
+        Esp32Communicator.updater()
 
     def on_disconnect(self, sid):
-        print('disconnect ', sid)
+        print('[ESP] disconnect ', sid)
 
     def on_change_switches(self, sid, data):
         response_data = {}
@@ -97,15 +167,30 @@ class Esp32Communicator(socketio.Namespace):
             switches[switch].change_switch_state()
             print("Weiche " + str(switch) + " stellen")
         CanCommunicator.send_switch_states()
-        for switch in data["data"]:
-            # response data only for testing
-            response_data[switches[switch].id] = switches[switch].target_pos
-        print("response: ", response_data)
-        sio.emit("update_switch_positions", {'data': response_data})
-        # sio.emit("state_change_request", {'data': data})
+        Esp32Communicator.send_switch_states()
 
+    @staticmethod
+    def updater():
+        Esp32Communicator.send_switch_states()
+        timer = threading.Timer(0.2, Esp32Communicator.updater)
+        timer.start()
 
-sio.register_namespace(Esp32Communicator())
+    @staticmethod
+    def send_switch_states():
+        CanCommunicator.request_inputs(0, 2, 0x38)
+        CanCommunicator.request_inputs(0, 2, 0x39)
+        data = {switch.id: switch.current_pos for switch in switches}
+        sio.emit("update_switch_positions", {'data': data})
+
 
 if __name__ == '__main__':
+    # initialize can Listener
+    if CAN_ENABLED:
+        bus = can.interface.Bus(channel=CanCommunicator.channel,
+                                bustype=CanCommunicator.bustype,
+                                bitrate=125000)
+        notifier = can.Notifier(bus, [CanCommunicator.receive_event])
+
+    # socketio to ESP32
+    sio.register_namespace(Esp32Communicator())
     eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
