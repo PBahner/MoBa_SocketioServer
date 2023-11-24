029db320eb2c02698667025d18d3296f474a0543
PBahner <kl.timo@outlook.de>
2023-11-24 22:11:31 +0100

    implement response to request of switch input-references - response with switch-input-references - initialize 'target_pos' if current pos is fetched - store 'servo_number' (id at servo module) and send target positions ordered by 'servo_number' - improve prints - change PCF8574A to PCF8574 addresses

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ class Switches:
 
     def __init__(self, switch_id,
                  servo_module,
+                 servo_number,
                  input_reference_plus=None,
                  input_reference_minus=None):
         self.__input_pin_minus = None
@@ -46,6 +47,7 @@ class Switches:
         self.target_pos = self.__pos[""]
         self.id = switch_id
         self.servo_module = servo_module
+        self.servo_number = servo_number
         self.input_reference_plus = input_reference_plus
         self.input_reference_minus = input_reference_minus
 
@@ -65,17 +67,21 @@ class Switches:
         else:
             self.current_pos = self.__pos[""]
 
+        # set target pos as soon current pos is fetched
+        if self.target_pos == self.__pos[""]:
+            self.target_pos = self.current_pos
+
     def change_switch_state(self):
         self.target_pos = not self.current_pos
 
 
-switches = [Switches(0, 0, PcfPinRef(0, 2, 0x39, 0), PcfPinRef(0, 2, 0x39, 1)),
-            Switches(1, 0, PcfPinRef(0, 2, 0x38, 7), PcfPinRef(0, 2, 0x38, 6)),
-            Switches(2, 0, PcfPinRef(0, 2, 0x38, 4), PcfPinRef(0, 2, 0x38, 5)),
-            Switches(3, 1),
-            Switches(4, 1),
-            Switches(5, 0, PcfPinRef(0, 2, 0x38, 2), PcfPinRef(0, 2, 0x38, 3)),
-            Switches(6, 0, PcfPinRef(0, 2, 0x38, 1), PcfPinRef(0, 2, 0x38, 0))]
+switches = [Switches(0, 0, 4, PcfPinRef(0, 2, 0x21, 0), PcfPinRef(0, 2, 0x21, 1)),
+            Switches(1, 0, 3, PcfPinRef(0, 2, 0x20, 7), PcfPinRef(0, 2, 0x20, 6)),
+            Switches(2, 0, 2, PcfPinRef(0, 2, 0x20, 4), PcfPinRef(0, 2, 0x20, 5)),
+            Switches(3, 1, 0),
+            Switches(4, 1, 1),
+            Switches(5, 0, 1, PcfPinRef(0, 2, 0x20, 2), PcfPinRef(0, 2, 0x20, 3)),
+            Switches(6, 0, 0, PcfPinRef(0, 2, 0x20, 1), PcfPinRef(0, 2, 0x20, 0))]
 
 
 class CanCommunicator:
@@ -102,14 +108,13 @@ class CanCommunicator:
 
     @staticmethod
     def send_switch_states():
-        switch_positions = [(s.servo_module, s.target_pos) for s in switches]
-        data = [[] for _ in range(max(p[0] for p in switch_positions) + 1)]
-        for module, pos in switch_positions:
-            data[module].append(pos)
-        data = [CanCommunicator.bools_to_bytes(p)[0] for p in data]
+        switch_positions = [(s.servo_module, s.servo_number, s.target_pos) for s in switches]
+        data = [0] * (max(p[0] for p in switch_positions) + 1)  # get list-size (=count of servo modules)
+        for module, s_number, pos in switch_positions:
+            data[module] += (pos << s_number)
         if CanCommunicator.send_msg(1, data):
             for switch in switches:
-                # only for testing
+                # only for testing (change switch position virtually)
                 if switch.input_reference_minus is None or switch.input_reference_plus is None or not CAN_ENABLED:
                     switch.current_pos = switch.target_pos
 
@@ -123,14 +128,15 @@ class CanCommunicator:
             msg = can.Message(arbitration_id=id, data=data, is_extended_id=False)
             try:
                 bus.send(msg)
-                print(f"[CAN] Message sent on {bus.channel_info} id: {msg.arbitration_id} msg: {msg.data}")
+                print(f"[CAN] Message sent on {bus.channel_info} id: {msg.arbitration_id} msg: {list(msg.data)}")
                 return True
             except can.CanError:
                 print("[CAN] Message NOT sent")
                 return False
 
     @staticmethod
-    def receive_event(msg):
+    def receive_event(msg):  # Inputs from IO-Modules
+        print("[CAN] received id:", msg.arbitration_id, "data", list(msg.data))
         if msg.arbitration_id == 11:
             module_id = msg.data[0] & 0x0F
             input_type = msg.data[0] >> 4
@@ -148,6 +154,16 @@ class CanCommunicator:
             response_data = {switch.id: switch.current_pos for switch in switches}
             print("[ESP] emit: update_switch_positions", response_data)
             sio.emit("update_switch_positions", {'data': response_data})
+
+        elif msg.arbitration_id == 3:  # Request: Switch-PCF-References
+            for requested_switch in msg.data:
+                for count, input_reference in enumerate([switches[requested_switch].input_reference_plus,
+                                                         switches[requested_switch].input_reference_minus]):
+                    data = [input_reference.input_module + input_reference.input_type << 4,
+                            input_reference.input_address,
+                            input_reference.input_pin,
+                            count + (requested_switch << 1)]  # first bit represents relevant switch state
+                    CanCommunicator.send_msg(2, data)
 
 
 class Esp32Communicator(socketio.Namespace):
@@ -176,8 +192,8 @@ class Esp32Communicator(socketio.Namespace):
 
     @staticmethod
     def send_switch_states():
-        CanCommunicator.request_inputs(0, 2, 0x38)
-        CanCommunicator.request_inputs(0, 2, 0x39)
+        CanCommunicator.request_inputs(0, 2, 0x20)
+        CanCommunicator.request_inputs(0, 2, 0x21)
         data = {switch.id: switch.current_pos for switch in switches}
         sio.emit("update_switch_positions", {'data': data})
 
