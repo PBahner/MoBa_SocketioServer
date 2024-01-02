5ac03aabac2951ccb60575cfee244ed8037320ca
PBahner <kl.timo@outlook.de>
2024-01-02 18:51:58 +0100

    refactoring - switch --> turnout - state --> position - PcfPinRef --> InputReference

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import socketio
 import eventlet
 eventlet.monkey_patch()
 
-sio = socketio.Server(cors_allowed_origins='*') #, logger=True, engineio_logger=True
+sio = socketio.Server(cors_allowed_origins='*')  # , logger=True, engineio_logger=True
 app = socketio.WSGIApp(sio)
 
 CAN_ENABLED = True
@@ -12,7 +12,7 @@ if CAN_ENABLED:
     import can
 
 
-class PcfPinRef:  # contains all required information/addresses to access an PCF8574 input
+class InputReference:  # contains all required information/addresses to access an PCF8574 input
     def __init__(self,
                  input_module,
                  input_type,
@@ -29,14 +29,14 @@ class PcfPinRef:  # contains all required information/addresses to access an PCF
                 input_address == self.input_address)
 
 
-class Switches:
+class Turnout:
     __pos = {
         "+": False,
         "-": True,
         "": 2,
     }
 
-    def __init__(self, switch_id,
+    def __init__(self, turnout_id,
                  servo_module,
                  servo_number,
                  input_reference_plus=None,
@@ -45,7 +45,7 @@ class Switches:
         self.__input_pin_plus = None
         self.current_pos = self.__pos[""]
         self.target_pos = self.__pos[""]
-        self.id = switch_id
+        self.id = turnout_id
         self.servo_module = servo_module
         self.servo_number = servo_number
         self.input_reference_plus = input_reference_plus
@@ -71,17 +71,17 @@ class Switches:
         if self.target_pos == self.__pos[""]:
             self.target_pos = self.current_pos
 
-    def change_switch_state(self):
+    def change_target_pos(self):
         self.target_pos = not self.current_pos
 
 
-switches = [Switches(0, 0, 4, PcfPinRef(0, 2, 0x21, 0), PcfPinRef(0, 2, 0x21, 1)),
-            Switches(1, 0, 3, PcfPinRef(0, 2, 0x20, 7), PcfPinRef(0, 2, 0x20, 6)),
-            Switches(2, 0, 2, PcfPinRef(0, 2, 0x20, 4), PcfPinRef(0, 2, 0x20, 5)),
-            Switches(3, 1, 0),
-            Switches(4, 1, 1),
-            Switches(5, 0, 1, PcfPinRef(0, 2, 0x20, 2), PcfPinRef(0, 2, 0x20, 3)),
-            Switches(6, 0, 0, PcfPinRef(0, 2, 0x20, 1), PcfPinRef(0, 2, 0x20, 0))]
+turnouts = [Turnout(0, 0, 4, InputReference(0, 2, 0x21, 0), InputReference(0, 2, 0x21, 1)),
+            Turnout(1, 0, 3, InputReference(0, 2, 0x20, 7), InputReference(0, 2, 0x20, 6)),
+            Turnout(2, 0, 2, InputReference(0, 2, 0x20, 4), InputReference(0, 2, 0x20, 5)),
+            Turnout(3, 1, 0),
+            Turnout(4, 1, 1),
+            Turnout(5, 0, 1, InputReference(0, 2, 0x20, 2), InputReference(0, 2, 0x20, 3)),
+            Turnout(6, 0, 0, InputReference(0, 2, 0x20, 1), InputReference(0, 2, 0x20, 0))]
 
 
 class CanCommunicator:
@@ -107,16 +107,16 @@ class CanCommunicator:
         CanCommunicator.send_msg(12, data)
 
     @staticmethod
-    def send_switch_states():
-        switch_positions = [(s.servo_module, s.servo_number, s.target_pos) for s in switches]
-        data = [0] * (max(p[0] for p in switch_positions) + 1)  # get list-size (=count of servo modules)
-        for module, s_number, pos in switch_positions:
+    def send_turnout_positions():
+        turnout_positions = [(s.servo_module, s.servo_number, s.target_pos) for s in turnouts]
+        data = [0] * (max(p[0] for p in turnout_positions) + 1)  # get list-size (=count of servo modules)
+        for module, s_number, pos in turnout_positions:
             data[module] += (pos << s_number)
         if CanCommunicator.send_msg(1, data):
-            for switch in switches:
-                # only for testing (change switch position virtually)
-                if switch.input_reference_minus is None or switch.input_reference_plus is None or not CAN_ENABLED:
-                    switch.current_pos = switch.target_pos
+            for turnout in turnouts:
+                # only for testing (change turnout position virtually)
+                if turnout.input_reference_minus is None or turnout.input_reference_plus is None or not CAN_ENABLED:
+                    turnout.current_pos = turnout.target_pos
 
     @staticmethod
     def send_msg(id, data):
@@ -141,35 +141,35 @@ class CanCommunicator:
             module_id = msg.data[0] & 0x0F
             input_type = msg.data[0] >> 4
             input_address = msg.data[1]
-            for switch in switches:
-                if switch.input_reference_plus is None or switch.input_reference_plus is None:
+            for turnout in turnouts:
+                if turnout.input_reference_plus is None or turnout.input_reference_plus is None:
                     continue
-                if switch.input_reference_minus.compare(module_id, input_type, input_address):
-                    input_pin = (msg.data[2] >> switch.input_reference_minus.input_pin) & 1
-                    switch.set_input_pin_minus(input_pin)
-                if switch.input_reference_plus.compare(module_id, input_type, input_address):
-                    input_pin = (msg.data[2] >> switch.input_reference_plus.input_pin) & 1
-                    switch.set_input_pin_plus(input_pin)
+                if turnout.input_reference_minus.compare(module_id, input_type, input_address):
+                    input_pin = (msg.data[2] >> turnout.input_reference_minus.input_pin) & 1
+                    turnout.set_input_pin_minus(input_pin)
+                if turnout.input_reference_plus.compare(module_id, input_type, input_address):
+                    input_pin = (msg.data[2] >> turnout.input_reference_plus.input_pin) & 1
+                    turnout.set_input_pin_plus(input_pin)
 
-            response_data = {switch.id: switch.current_pos for switch in switches}
+            response_data = {turnout.id: turnout.current_pos for turnout in turnouts}
             print("[ESP] emit: update_switch_positions", response_data)
             sio.emit("update_switch_positions", {'data': response_data})
 
-        elif msg.arbitration_id == 3:  # Request: Switch-PCF-References
-            for requested_switch in msg.data:
-                for count, input_reference in enumerate([switches[requested_switch].input_reference_plus,
-                                                         switches[requested_switch].input_reference_minus]):
+        elif msg.arbitration_id == 3:  # request: turnout input-references
+            for requested_turnout in msg.data:
+                for count, input_reference in enumerate([turnouts[requested_turnout].input_reference_plus,
+                                                         turnouts[requested_turnout].input_reference_minus]):
                     data = [input_reference.input_module + input_reference.input_type << 4,
                             input_reference.input_address,
                             input_reference.input_pin,
-                            count + (requested_switch << 1)]  # first bit represents relevant switch state
+                            count + (requested_turnout << 1)]  # first bit represents relevant turnout position
                     CanCommunicator.send_msg(2, data)
 
 
 class Esp32Communicator(socketio.Namespace):
     def on_connect(self, sid, environ, auth):
         print('[ESP] connect ', sid)
-        data = {switch.id: switch.current_pos for switch in switches}
+        data = {turnout.id: turnout.current_pos for turnout in turnouts}
         sio.emit("init_switch_positions", {'data': data})
         print("[ESP] emit: init_switch_positions", data)
         Esp32Communicator.updater()
@@ -177,24 +177,24 @@ class Esp32Communicator(socketio.Namespace):
     def on_disconnect(self, sid):
         print('[ESP] disconnect ', sid)
 
-    def on_change_switches(self, sid, data):
-        for switch in data["data"]:
-            switches[switch].change_switch_state()
-            print("Weiche " + str(switch) + " stellen")
-        CanCommunicator.send_switch_states()
-        Esp32Communicator.send_switch_states()
+    def on_change_turnouts(self, sid, data):
+        for turnout in data["data"]:
+            turnouts[turnout].change_target_pos()
+            print("Weiche " + str(turnout) + " stellen")
+        CanCommunicator.send_turnout_positions()
+        Esp32Communicator.send_turnout_positions()
 
     @staticmethod
     def updater():
-        Esp32Communicator.send_switch_states()
+        Esp32Communicator.send_turnout_positions()
         timer = threading.Timer(0.2, Esp32Communicator.updater)
         timer.start()
 
     @staticmethod
-    def send_switch_states():
+    def send_turnout_positions():
         CanCommunicator.request_inputs(0, 2, 0x20)
         CanCommunicator.request_inputs(0, 2, 0x21)
-        data = {switch.id: switch.current_pos for switch in switches}
+        data = {turnout.id: turnout.current_pos for turnout in turnouts}
         sio.emit("update_switch_positions", {'data': data})
 
 
